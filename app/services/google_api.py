46170cc7d0895d5a87a776f15a447d53441e4e6a
thalq <95490979+thalq@users.noreply.github.com>
2022-12-09 11:44:31 +0300

    Добавила проверку - проверяет хватит ли места в таблице для данных

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -1,34 +1,44 @@
 from datetime import datetime
+from http import HTTPStatus
 
 from aiogoogle import Aiogoogle
+from fastapi import HTTPException
 
 from app.core.config import settings
 from app.models import CharityProject
 
 FORMAT = "%Y/%m/%d %H:%M:%S"
 NOW_DATE_TIME = datetime.now().strftime(FORMAT)
+ROW_COUNT = 100
+COLUMN_COUNT = 11
 TABLE_VALUES = [
         ['Отчет от', NOW_DATE_TIME],
         ['Топ проектов по скорости закрытия'],
         ['Название проекта', 'Время сбора', 'Описание']
     ]
 
-
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
     service = await wrapper_services.discover('sheets', 'v4')
     spreadsheet_body = {
-        'properties': {'title': f'Отчет от {NOW_DATE_TIME}',
-                       'locale': 'ru_RU'},
-        'sheets': [{'properties': {'sheetType': 'GRID',
-                                   'sheetId': 0,
-                                   'title': 'Лист1',
-                                   'gridProperties': {'rowCount': 100,
-                                                      'columnCount': 11}}}]
+        'properties': {
+            'title': f'Отчет от {NOW_DATE_TIME}',
+            'locale': 'ru_RU',
+        },
+        'sheets': [{
+            'properties': {
+                'sheetType': 'GRID',
+                'sheetId': 0,
+                'title': 'Лист1',
+                'gridProperties': {
+                    'rowCount': ROW_COUNT,
+                    'columnCount': COLUMN_COUNT,
+                }
+            }
+        }]
     }
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
     )
-    print(dir(response))
     spreadsheet_id = response['spreadsheetId']
     return spreadsheet_id
 
@@ -55,7 +65,6 @@ async def spreadsheets_update_value(
         wrapper_services: Aiogoogle
 ) -> None:
     service = await wrapper_services.discover('sheets', 'v4')
-    # Здесь формируется тело таблицы
     for project in projects:
         new_row = [
             str(project.name),
@@ -67,6 +76,14 @@ async def spreadsheets_update_value(
         'majorDimension': 'ROWS',
         'values': TABLE_VALUES
     }
+    rows = len(TABLE_VALUES)
+
+    if rows > ROW_COUNT:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Данных больше, чем места в таблице',
+        )
+
     await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
             spreadsheetId=spreadsheet_id,
